4684069a60feb2500847f3ebb690df46db1aac13
Fitsum <fitbez@gmail.com>
2022-09-22 22:35:35 -0400

    added auth to the routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const productRoute = require("./routes/product");
+const userRoute = require("./routes/auth");
 const cors = require("cors");
 
 const app = express();
-const productRoute = require("./routes/product");
 
+/** connecting our database with our app */
 mongoose
-  .connect(process.env.MOGODB_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("Database connection is successfull");
+    console.log("Database connection is successful!");
   })
   .catch((err) => {
     console.log(err);
@@ -18,7 +20,9 @@ mongoose
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use("/api/products", productRoute);
+app.use("/api/user", userRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is up and running on port 5000");
+/** setting up server */
+app.listen(5000, () => {
+  console.log("Our server is up and running on port 5000");
 });
